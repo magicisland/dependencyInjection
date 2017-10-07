ed4d59ea3396ebeb5e0d2d55a4128ecdf73c7f21
magicisland <marcelojavierrodriguez@gmail.com>
2017-10-07 10:18:26 -0300

    properties dependency injection

---
+++ src/main/java/org/javabrains/koushik/Triangle.java
@@ -0,0 +1,27 @@
+package org.javabrains.koushik;
+
+public class Triangle {
+
+	protected String type;
+	
+	
+	
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
+	public void draw(){
+		
+		System.out.println(" Triangle draw " + getType());
+		
+		
+	}
+}
